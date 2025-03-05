fba6293fe02694a4bd1c01de75d73aea45229a43
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2025-03-05 10:57:55 -0500

    Added board and started working on BP version of grid map

--- Source/PacManRemake/PacManRemake.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "PacManRemake.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PacManRemake, "PacManRemake" );
